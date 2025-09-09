444cbb5bbd81ae144e77f483fd7075823a49d27c
wattry <09wattry@gmail.com>
2025-09-09 07:41:04 -0700

    Update url

--- packages/app/docusaurus.config.ts
+++ packages/app/docusaurus.config.ts
@@ -51,10 +51,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://galvanized-solutions.github.io',
+  url: 'https://claude.refers.us',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cookbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
